194e61a865d15f9160df0414e3f2e0baa555cf66
Thilina Ashen Gamage <ashen.workplace@gmail.com>
2024-10-07 14:39:23 +0530

    [server] add multer for upload api

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,38 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import multer from "multer";
+import fs from 'fs';
+
+const upload = multer({ dest: 'uploads/' })
+
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello from Express Backend!");
+    res.json({
+       message: "Hello from Express Backend!",
+       success: true
+    });
 });
 
-const PORT = process.env.PORT || 3000;
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.status(200)
+})
+
+app.post('/upload', upload.single('document'),(req , res) => {
+    console.log(req.file, req.body)
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
