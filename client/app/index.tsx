ec298aa36ff3985c0cbbc8b9e514ceae4a7102ca
Thilina Ashen Gamage <ashen.workplace@gmail.com>
2024-10-07 15:13:47 +0530

    [client] modify home ui

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Welcome to Expo App</Text>
+            <Text style={styles.subtitle}>Upload Demo Screen</Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F4F6FA',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: '600',
+        color: '#1F2D56',
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontSize: 18,
+        fontWeight: '400',
+        color: '#4C5C7E',
+    },
+});
